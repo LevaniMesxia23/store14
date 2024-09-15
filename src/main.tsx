41a6a997223414c10a0b1268f41e0aa3c02efa02
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-09-15 04:58:21 +0400

    fix: fix bug

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './i18n';
-import ReactDOM from 'react-dom/client';
 
 
 createRoot(document.getElementById('root')!).render(
