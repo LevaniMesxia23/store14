427b6a8aaf29c2417843d10655617703f1eecd7d
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-09-16 21:54:38 +0400

    comment i18n

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import './i18n';
+// import './i18n';
 import { StrictMode } from 'react';
 
 createRoot(document.getElementById('root')!).render(
