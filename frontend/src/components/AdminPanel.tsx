cc0d1fd7b5b6f563e6a63212a012a351bbf5937a
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-09-19 01:05:05 +0400

    feat: update backand folder stucture

--- frontend/src/components/AdminPanel.tsx
+++ frontend/src/components/AdminPanel.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Swal from 'sweetalert2';
 
-
 const AdminPanel: React.FC = () => {
   const [productName, setProductName] = useState('');
   const [productPrice, setProductPrice] = useState('');
